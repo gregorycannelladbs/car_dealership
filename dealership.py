7da06f077eca97dc9f28c7c0952401a814341371
gregorycannelladbs <greg.cannella@gmail.com>
2020-04-13 10:48:27 +0100

    first final version for which everything works !

--- dealership.py
+++ dealership.py
@@ -2,7 +2,6 @@ from car import Car, ElectricCar, PetrolCar, DieselCar, HybridCar
 import csv
 import random
 import string
-import numpy as np
 
 #test_car = PetrolCar('black', 'peugeot', 3000, 1.4, 'in stock', '07LH1559', 5)
 
@@ -73,36 +72,45 @@ class Dealership(object):
         return len(rented_cars)
 
     def get_data(self, car_list):      
-        data = []
+        if car_list == self.electric_cars:
+            data = [['Index', 'Make', 'Colour', 'Mileage', 'Engine_size', 'Status', 'Reg_number', 'Fuel_cells', 'Car_type']]
+        
+        elif car_list == self.petrol_cars or car_list == self.diesel_cars:
+            data = [['Index', 'Make', 'Colour', 'Mileage', 'Engine_size', 'Status', 'Reg_number', 'Cylinders', 'Car_type']]
+        
+        elif car_list == self.hybrid_cars:
+            data = [['Index', 'Make', 'Colour', 'Mileage', 'Engine_size', 'Status', 'Reg_number', 'Fuel_cells', 'Cylinders', 
+                    'Car_type']]
+            
         for car in car_list:
             values = [str(value) for value in car.__dict__.values()]
             values.insert(0, str(car_list.index(car)))
             data.append(values)
-        data = np.array(data)
-        data = data[np.argsort(data[:, 1])]
+            
         return data
         
     def display_data(self, car_list):
         data = self.get_data(car_list)
+
         if car_list == self.electric_cars:
-            print('\n{:<10s} {:<15s} {:<10s} {:<10s} {:<15s} {:<15s} {:<15s} {:<15s} {:<15s}'.format('Index', 'Make', 'Colour', 
-                  'mileage', 'Engine_size', 'Status', 'Reg_number', 'Fuel_cells', 'Car_type'))
+#            print('\n{:<10s} {:<15s} {:<10s} {:<10s} {:<15s} {:<15s} {:<15s} {:<15s} {:<15s}'.format('Index', 'Make', 'Colour', 
+#                  'Mileage', 'Engine_size', 'Status', 'Reg_number', 'Fuel_cells', 'Car_type'))
             
             for i in data:
                 print('{:<10s} {:<15s} {:<10s} {:<10s} {:<15s} {:<15s} {:<15s} {:<15s} {:<15s}'.format(i[0], i[1], i[2], i[3],
                           i[4], i[5], i[6], i[7], i[8]))
         
         elif car_list == self.petrol_cars or car_list == self.diesel_cars:
-            print('\n{:<10s} {:<15s} {:<10s} {:<10s} {:<15s} {:<15s} {:<15s} {:<15s} {:<15s}'.format('Index', 'Make', 'Colour', 
-                  'mileage', 'Engine_size', 'Status', 'Reg_number', 'Cylinders', 'Car_type'))
+#            print('\n{:<10s} {:<15s} {:<10s} {:<10s} {:<15s} {:<15s} {:<15s} {:<15s} {:<15s}'.format('Index', 'Make', 'Colour', 
+#                  'Mileage', 'Engine_size', 'Status', 'Reg_number', 'Cylinders', 'Car_type'))
             
             for i in data:
                 print('{:<10s} {:<15s} {:<10s} {:<10s} {:<15s} {:<15s} {:<15s} {:<15s} {:<15s}'.format(i[0], i[1], i[2], i[3],
                           i[4], i[5], i[6], i[7], i[8]))
                 
         elif car_list == self.hybrid_cars:
-            print('\n{:<10s} {:<15s} {:<10s} {:<10s} {:<15s} {:<15s} {:<15s} {:<15s} {:<15s} {:<15s}'.format('Index', 'Make', 
-                  'Colour', 'mileage', 'Engine_size', 'Status', 'Reg_number', 'Fuel_cells', 'Cylinders', 'Car_type'))
+#            print('\n{:<10s} {:<15s} {:<10s} {:<10s} {:<15s} {:<15s} {:<15s} {:<15s} {:<15s} {:<15s}'.format('Index', 'Make', 
+#                  'Colour', 'Mileage', 'Engine_size', 'Status', 'Reg_number', 'Fuel_cells', 'Cylinders', 'Car_type'))
                 
             for i in data:
                 print('{:<10s} {:<15s} {:<10s} {:<10s} {:<15s} {:<15s} {:<15s} {:<15s} {:<15s} {:<15s}'.format(i[0], i[1], i[2], 
@@ -127,76 +135,149 @@ class Dealership(object):
         else:
             return self.hybrid_cars
     
-#    def save_original_data(self):
-#            self.create_current_stock()
-#            file_names = ['electric_cars', 'petrol_cars', 'diesel_cars', 'hybrid_cars']
-#            index = 0
-#            for i in (self.get_data(self.electric_cars), self.get_data(self.petrol_cars),
-#                      self.get_data(self.diesel_cars), self.get_data(self.hybrid_cars)):
-#                file_name = file_names[index] + '.csv'
-#                index += 1
-#                with open(file_name, "w", newline="",  encoding='utf-8') as f:
-#                    writer = csv.writer(f)
-#                    writer.writerows(i)
-#                    f.close()
+    def save_original_data(self):
+            file_names = ['electric_cars', 'petrol_cars', 'diesel_cars', 'hybrid_cars']
+            index = 0
+            data_electric = self.get_data(self.electric_cars)
+            data_petrol = self.get_data(self.petrol_cars)
+            data_diesel = self.get_data(self.diesel_cars)
+            data_hybrid = self.get_data(self.hybrid_cars)
+            for i in (data_electric, data_petrol, data_diesel, data_hybrid):
+                data = [item[1:] for item in i]
+                file_name = file_names[index] + '.csv'
+                index += 1
+                with open(file_name, "w", newline="",  encoding='utf-8') as f:
+                    writer = csv.writer(f)
+                    writer.writerows(data)
+                    f.close()
     
-#    def load_data(self):
-#            file_names = ['electric_cars', 'petrol_cars', 'diesel_cars', 'hybrid_cars']
-#            index = 0
-#            for i in (self.get_data(self.electric_cars), self.get_data(self.petrol_cars),
+    def load_data(self):
+            file_names = ['electric_cars', 'petrol_cars', 'diesel_cars', 'hybrid_cars']
+            index = 0
+            for i in file_names: 
+#                      self.get_data(self.petrol_cars),
 #                      self.get_data(self.diesel_cars), self.get_data(self.hybrid_cars)):
-#                file_name = file_names[index] + '.csv'
-#                index += 1
-#                with open(file_name, newline='') as f:
-#                    reader = csv.reader(f)
-#                    next(reader, None)  # Skip the header.
-#                    # Unpack the row directly in the head of the for loop.
-#                    for name, score1, score2, rank in reader:
-#                        # Convert the numbers to floats.
-#                        score1 = float(score1)
-#                        score2 = float(score2)
-#                        rank = float(rank)
-#                        # Now create the Student instance and append it to the list.
-#                        i.append(Student(name, (score1, score2), rank))
-
+                file_name = file_names[index] + '.csv'
+                index += 1
+                with open(file_name, newline='') as f:
+                    reader = csv.reader(f)
+                    next(reader, None)  # Skip the header.
+                    # Unpack the row directly in the head of the for loop.           
+                    if file_name == 'electric_cars.csv':
+                        for Make, Colour, Mileage, Engine_size, Status, Reg_number, Fuel_cells, Car_type in reader:
+                            # Now create the car instance and append it to the list.
+                            car = ElectricCar()
+                            car.setMake(Make)
+                            car.setColour(Colour)
+                            car.setMileage(Mileage)
+                            car.setEngineSize(Engine_size)
+                            car.setStatus(Status)
+                            car.setRegNumber(Reg_number)
+                            car.setNumberFuelCells(Fuel_cells)
+                            car.setCarType(Car_type)
+                            self.electric_cars.append(car)
+                            
+                    elif file_name in ('petrol_cars.csv', 'diesel_cars.csv'):
+                        for Make, Colour, Mileage, Engine_size, Status, Reg_number, Cylinders, Car_type in reader:
+                            # Now create the car instance and append it to the list.
+                            if file_name == 'petrol_cars.csv':
+                                car = PetrolCar()
+                                
+                            elif file_name == 'diesel_cars.csv':
+                                car = DieselCar()
+                            
+                            car.setMake(Make)
+                            car.setColour(Colour)
+                            car.setMileage(Mileage)
+                            car.setEngineSize(Engine_size)
+                            car.setStatus(Status)
+                            car.setRegNumber(Reg_number)
+                            car.setNumberCylinders(Cylinders)
+                            car.setCarType(Car_type)
+                            
+                            if file_name == 'petrol_cars.csv':
+                                self.petrol_cars.append(car)
+                                
+                            elif file_name == 'diesel_cars.csv':
+                                self.diesel_cars.append(car)
+                
+                            
+#                            elif file_name == 'diesel_cars.csv':
+#                                car = DieselCar() 
+#                                car.setMake(Make)
+#                                car.setColour(Colour)
+#                                car.setMileage(Mileage)
+#                                car.setEngineSize(Engine_size)
+#                                car.setStatus(Status)
+#                                car.setRegNumber(Reg_number)
+#                                car.setNumberCylinders(Cylinders)
+#                                car.setCarType(Car_type)
+#                                self.diesel_cars.append(car)
+                            
+#                            if file_name == 'petrol_cars.csv':
+#                                self.petrol_cars.append(car)
+#                                
+#                            elif file_name == 'diesel_cars.csv':
+#                                self.diesel_cars.append(car)
+                    
+                    elif file_name in ('hybrid_cars.csv'):
+                        for Make, Colour, Mileage, Engine_size, Status, Reg_number, Fuel_cells, Cylinders, Car_type in reader:
+                            # Now create the car instance and append it to the list.
+                            car = HybridCar()
+                            car.setMake(Make)
+                            car.setColour(Colour)
+                            car.setMileage(Mileage)
+                            car.setEngineSize(Engine_size)
+                            car.setStatus(Status)
+                            car.setRegNumber(Reg_number)
+                            car.setNumberFuelCells(Fuel_cells)
+                            car.setNumberCylinders(Cylinders)
+                            car.setCarType(Car_type)
+                            self.hybrid_cars.append(car)
+                            
     def main():
         dealership = Dealership()
         answer = input('\nDo you want to start from scratch "1", or load previously saved data "2"? ')                    
         if answer == '1':
             dealership.create_current_stock()
-            proceed = 'y'
-            while proceed == 'y':
-                answer = input('\nWould you like to rent "r" or return "b" a car? ')
-                if answer == 'r':
-                    status = 'rented'
-                    car_type = input('\nWhat type would you like to rent? p/e/d/h ')
-                    car_list = dealership.chose_car_list(car_type)
-                    dealership.display_data(car_list)
-                    index = eval(input('\nEnter the index of the car you want to rent: '))
-                    dealership.change_status(car_list, status, index)
-                
-                else:
-                    status = 'in stock'
-                    mileage = eval(input('\nWhat is the mileage of the car on return? '))
-                    car_list = dealership.chose_car_list()
-                    dealership.display_data(car_list)
-                    index = eval(input('\nEnter the index of the car you want to return: '))
-                    dealership.change_status(car_list, status, index)
-                    dealership.change_mileage(car_list, mileage, index) 
-                
-                answer = input('\nWould you like to save changes made? y/n ')
-                if answer == 'y':
-                    data = dealership.get_data(car_list)
-                    file_name = car_list[0].getCarType() + '_cars.csv'
-                    with open(file_name, "w", newline="",  encoding='utf-8') as f:
-                        writer = csv.writer(f)
-                        writer.writerows(data)
-                        f.close()
-            
-                proceed = input('\nWould you like to continue y/n? ')
-            
+            dealership.save_original_data()
+        
         else:
-            print('\nSorry this feature is not available yet. I am working hard to make this feature available in a near future')
+            dealership.load_data()
+            
+        proceed = 'y'
+        while proceed == 'y':
+            answer = input('\nWould you like to rent "r" or return "b" a car? ')
+            if answer == 'r':
+                status = 'rented'
+                car_type = input('\nWhat type would you like to rent? p/e/d/h ')
+                car_list = dealership.chose_car_list(car_type)
+                dealership.display_data(car_list)
+                index = eval(input('\nEnter the index of the car you want to rent: '))
+                dealership.change_status(car_list, status, index)
+            
+            else:
+                status = 'in stock'
+                car_type = input('\nWhat type would you like to rent? p/e/d/h ')
+                mileage = eval(input('\nWhat is the mileage of the car on return? '))
+                car_list = dealership.chose_car_list(car_type)
+                dealership.display_data(car_list)
+                index = eval(input('\nEnter the index of the car you want to return: '))
+                dealership.change_status(car_list, status, index)
+                dealership.change_mileage(car_list, mileage, index) 
+            
+            answer = input('\nWould you like to save changes made? y/n ')
+            if answer == 'y':
+                data = dealership.get_data(car_list)
+                data = [item[1:] for item in data]
+                file_name = car_list[0].getCarType() + '_cars.csv'
+                with open(file_name, "w", newline="",  encoding='utf-8') as f:
+                    writer = csv.writer(f)
+                    writer.writerows(data)
+                    f.close()
+            
+            proceed = input('\nWould you like to continue y/n? ')
+            
                 
 #            file_names = ['electric_cars', 'petrol_cars', 'diesel_cars', 'hybrid_cars']
 #            for i in (dealership.get_data(dealership.electric_cars), dealership.get_data(dealership.petrol_cars),
