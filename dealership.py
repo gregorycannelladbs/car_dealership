8d509781d593ea1a9e8f0043f76de2927fe99ffe
gregorycannelladbs <greg.cannella@gmail.com>
2020-04-13 16:30:19 +0100

    final version

--- dealership.py
+++ dealership.py
@@ -12,19 +12,8 @@ class Dealership(object):
         self.petrol_cars = []
         self.diesel_cars = []
         self.hybrid_cars = []
-        
-    def get_electric_cars(self):
-        return self.electric_cars
-    
-    def get_petrol_cars(self):
-        return self.petrol_cars
-    
-    def get_diesel_cars(self):
-        return self.diesel_cars
-    
-    def get_hybrid_cars(self):
-        return self.hybrid_cars
-    
+
+# create original stock when using the application for the first time
     def create_current_stock(self):
         colours = ['blue', 'white', 'red', 'black', 'orange'] * 8
         makes = ['jaguar', 'lamborghini', 'ferrari', 'mustang', 'maserati'] * 8
@@ -63,14 +52,7 @@ class Dealership(object):
            self.hybrid_cars[i].setRegNumber(reg_numbers.pop())
            self.hybrid_cars[i].setNumberCylinders(number_cylinders.pop())
 
-    def in_stock(self, car_list):
-        cars_in_stock = [car for car in car_list if car.getStatus() == 'in stock']
-        return cars_in_stock
-    
-    def rented_cars(self, car_list):
-        rented_cars = [car for car in car_list if car.getStatus() == 'rented']
-        return len(rented_cars)
-
+# get the car attribute values of a list of cars + the header
     def get_data(self, car_list):      
         if car_list == self.electric_cars:
             data = [['Index', 'Make', 'Colour', 'Mileage', 'Engine_size', 'Status', 'Reg_number', 'Fuel_cells', 'Car_type']]
@@ -88,40 +70,38 @@ class Dealership(object):
             data.append(values)
             
         return data
-        
+
+# Get the data attributes of a car list in the correct shape and display it to the screen        
     def display_data(self, car_list):
         data = self.get_data(car_list)
 
         if car_list == self.electric_cars:
-#            print('\n{:<10s} {:<15s} {:<10s} {:<10s} {:<15s} {:<15s} {:<15s} {:<15s} {:<15s}'.format('Index', 'Make', 'Colour', 
-#                  'Mileage', 'Engine_size', 'Status', 'Reg_number', 'Fuel_cells', 'Car_type'))
-            
             for i in data:
                 print('{:<10s} {:<15s} {:<10s} {:<10s} {:<15s} {:<15s} {:<15s} {:<15s} {:<15s}'.format(i[0], i[1], i[2], i[3],
                           i[4], i[5], i[6], i[7], i[8]))
         
         elif car_list == self.petrol_cars or car_list == self.diesel_cars:
-#            print('\n{:<10s} {:<15s} {:<10s} {:<10s} {:<15s} {:<15s} {:<15s} {:<15s} {:<15s}'.format('Index', 'Make', 'Colour', 
-#                  'Mileage', 'Engine_size', 'Status', 'Reg_number', 'Cylinders', 'Car_type'))
-            
             for i in data:
                 print('{:<10s} {:<15s} {:<10s} {:<10s} {:<15s} {:<15s} {:<15s} {:<15s} {:<15s}'.format(i[0], i[1], i[2], i[3],
                           i[4], i[5], i[6], i[7], i[8]))
                 
         elif car_list == self.hybrid_cars:
-#            print('\n{:<10s} {:<15s} {:<10s} {:<10s} {:<15s} {:<15s} {:<15s} {:<15s} {:<15s} {:<15s}'.format('Index', 'Make', 
-#                  'Colour', 'Mileage', 'Engine_size', 'Status', 'Reg_number', 'Fuel_cells', 'Cylinders', 'Car_type'))
-                
             for i in data:
                 print('{:<10s} {:<15s} {:<10s} {:<10s} {:<15s} {:<15s} {:<15s} {:<15s} {:<15s} {:<15s}'.format(i[0], i[1], i[2], 
                       i[3], i[4], i[5], i[6], i[7], i[8], i[9]))
     
+# Change the status of a car. A car is either 'rented' or 'in stock' 
     def change_status(self, car_list, status, index): 
-        car_list[index].setStatus(status)
-        
+        if car_list[index].getStatus() == status:
+            print('\nSorry the car is already either Rented or In Stock')
+        else:
+            car_list[index].setStatus(status)
+
+#  Change the mileage of a car        
     def change_mileage(self, car_list, mileage, index):
         car_list[index].setMileage(mileage)
-        
+
+# Based on user input return the correct list of cars         
     def chose_car_list(self, car_type):
         if car_type == 'e':
             return self.electric_cars
@@ -134,7 +114,9 @@ class Dealership(object):
         
         else:
             return self.hybrid_cars
-    
+
+# Save the original data when the app is launched for the first time.
+# The user needes to select option '1' when launching the app for the first time.    
     def save_original_data(self):
             file_names = ['electric_cars', 'petrol_cars', 'diesel_cars', 'hybrid_cars']
             index = 0
@@ -150,13 +132,12 @@ class Dealership(object):
                     writer = csv.writer(f)
                     writer.writerows(data)
                     f.close()
-    
+
+# Load data if the app has been used at least once in the past     
     def load_data(self):
             file_names = ['electric_cars', 'petrol_cars', 'diesel_cars', 'hybrid_cars']
             index = 0
             for i in file_names: 
-#                      self.get_data(self.petrol_cars),
-#                      self.get_data(self.diesel_cars), self.get_data(self.hybrid_cars)):
                 file_name = file_names[index] + '.csv'
                 index += 1
                 with open(file_name, newline='') as f:
@@ -179,7 +160,6 @@ class Dealership(object):
                             
                     elif file_name in ('petrol_cars.csv', 'diesel_cars.csv'):
                         for Make, Colour, Mileage, Engine_size, Status, Reg_number, Cylinders, Car_type in reader:
-                            # Now create the car instance and append it to the list.
                             if file_name == 'petrol_cars.csv':
                                 car = PetrolCar()
                                 
@@ -200,29 +180,9 @@ class Dealership(object):
                                 
                             elif file_name == 'diesel_cars.csv':
                                 self.diesel_cars.append(car)
-                
-                            
-#                            elif file_name == 'diesel_cars.csv':
-#                                car = DieselCar() 
-#                                car.setMake(Make)
-#                                car.setColour(Colour)
-#                                car.setMileage(Mileage)
-#                                car.setEngineSize(Engine_size)
-#                                car.setStatus(Status)
-#                                car.setRegNumber(Reg_number)
-#                                car.setNumberCylinders(Cylinders)
-#                                car.setCarType(Car_type)
-#                                self.diesel_cars.append(car)
-                            
-#                            if file_name == 'petrol_cars.csv':
-#                                self.petrol_cars.append(car)
-#                                
-#                            elif file_name == 'diesel_cars.csv':
-#                                self.diesel_cars.append(car)
                     
                     elif file_name in ('hybrid_cars.csv'):
                         for Make, Colour, Mileage, Engine_size, Status, Reg_number, Fuel_cells, Cylinders, Car_type in reader:
-                            # Now create the car instance and append it to the list.
                             car = HybridCar()
                             car.setMake(Make)
                             car.setColour(Colour)
@@ -256,45 +216,27 @@ class Dealership(object):
                 index = eval(input('\nEnter the index of the car you want to rent: '))
                 dealership.change_status(car_list, status, index)
             
-            else:
+            elif answer == 'b':
                 status = 'in stock'
-                car_type = input('\nWhat type would you like to rent? p/e/d/h ')
-                mileage = eval(input('\nWhat is the mileage of the car on return? '))
+                car_type = input('\nWhat type would you like to return? p/e/d/h ')
                 car_list = dealership.chose_car_list(car_type)
                 dealership.display_data(car_list)
                 index = eval(input('\nEnter the index of the car you want to return: '))
                 dealership.change_status(car_list, status, index)
+                mileage = eval(input('\nWhat is the mileage of the car on return? '))
                 dealership.change_mileage(car_list, mileage, index) 
             
-            answer = input('\nWould you like to save changes made? y/n ')
-            if answer == 'y':
-                data = dealership.get_data(car_list)
-                data = [item[1:] for item in data]
-                file_name = car_list[0].getCarType() + '_cars.csv'
-                with open(file_name, "w", newline="",  encoding='utf-8') as f:
-                    writer = csv.writer(f)
-                    writer.writerows(data)
-                    f.close()
+                answer = input('\nWould you like to save changes made? y/n ')
+                if answer == 'y':
+                    data = dealership.get_data(car_list)
+                    data = [item[1:] for item in data]
+                    file_name = car_list[0].getCarType() + '_cars.csv'
+                    with open(file_name, "w", newline="",  encoding='utf-8') as f:
+                        writer = csv.writer(f)
+                        writer.writerows(data)
+                        f.close()
             
             proceed = input('\nWould you like to continue y/n? ')
-            
-                
-#            file_names = ['electric_cars', 'petrol_cars', 'diesel_cars', 'hybrid_cars']
-#            for i in (dealership.get_data(dealership.electric_cars), dealership.get_data(dealership.petrol_cars),
-#                      dealership.get_data(dealership.petrol_cars), dealership.get_data(dealership.hybrid_cars)):
-#                with open(file_names[index], newline='') as f:
-#                    reader = csv.reader(f)
-#                    next(reader, None)  # Skip the header.
-#                    # Unpack the row directly in the head of the for loop.
-#                    for name, score1, score2, rank in reader:
-#                        # Convert the numbers to floats.
-#                        score1 = float(score1)
-#                        score2 = float(score2)
-#                        rank = float(rank)
-#                        # Now create the Student instance and append it to the list.
-#                        i.append(Student(name, (score1, score2), rank))
-                    
-
-            
+                           
 if __name__ == '__main__':
    Dealership.main()
